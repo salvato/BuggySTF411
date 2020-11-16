3fb79022e2f7ed4932d92a96fd539818750b85b3
Gabriele Salvato <gabriele.salvato@cnr.it>
2020-11-16 17:25:59 +0100

    wip

--- Src/main.cpp
+++ Src/main.cpp
@@ -659,6 +659,95 @@ USART2_IRQHandler(void) { // defined in file "startup_stm32f411xe.s"
     HAL_UART_IRQHandler(&huart2);
 }
 
+/*
+uint32_t IC_Val1 = 0;
+uint32_t IC_Val2 = 0;
+uint32_t Difference = 0;
+bool     bFirstCaptured = false;  // is the first value captured ?
+uint8_t  Distance  = 0;
+TIM_HandleTypeDef htim1;
+
+#define TRIG_PIN  GPIO_PIN_8
+#define TRIG_PORT GPIOA
+
+static void
+MX_TIM1_Init(void) {
+    TIM_MasterConfigTypeDef sMasterConfig;
+    TIM_IC_InitTypeDef sConfigIC;
+    memset(&sMasterConfig, 0, sizeof(sMasterConfig));
+    memset(&sConfigIC, 0, sizeof(sConfigIC));
+
+    htim1.Instance = TIM1;
+    htim1.Init.Prescaler = 72-1;
+    htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim1.Init.Period = 0xffff-1;
+    htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim1.Init.RepetitionCounter = 0;
+    htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+    if(HAL_TIM_IC_Init(&htim1) != HAL_OK) {
+        Error_Handler();
+    }
+    sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+    sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+    if(HAL_TIMEx_MasterConfigSynchronization(&htim1, &sMasterConfig) != HAL_OK) {
+        Error_Handler();
+    }
+    sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
+    sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
+    sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
+    sConfigIC.ICFilter = 0;
+    if(HAL_TIM_IC_ConfigChannel(&htim1, &sConfigIC, TIM_CHANNEL_1) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+void
+HAL_TIM_IC_MspInit(TIM_HandleTypeDef* htim_ic) {
+    GPIO_InitTypeDef GPIO_InitStruct;
+    memset(&GPIO_InitStruct, 0, sizeof(GPIO_InitStruct));
+    if(htim_ic->Instance == TIM1) {
+        __HAL_RCC_TIM1_CLK_ENABLE();
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        // TIM1 GPIO Configuration
+        // PA8    ------> TIM1_CH1
+        GPIO_InitStruct.Pin = TRIG_PIN;
+        GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(TRIG_PORT, &GPIO_InitStruct);
+        // TIM1 interrupt Init
+        HAL_NVIC_SetPriority(TIM1_CC_IRQn, 0, 0);
+        HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
+    }
+}
+
+
+void
+HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim) {
+    if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1) { // if the interrupt source is channel1
+        if(!bFirstCaptured) { // if the first value is not captured
+            IC_Val1 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1); // read the first value
+            bFirstCaptured = true;  // set the first captured as true
+            // Now change the polarity to falling edge
+            __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_FALLING);
+        }
+        else if(bFirstCaptured) { // if the first is already captured
+            IC_Val2 = HAL_TIM_ReadCapturedValue(htim, TIM_CHANNEL_1);  // read second value
+            __HAL_TIM_SET_COUNTER(htim, 0);  // reset the counter
+            if(IC_Val2 > IC_Val1) {
+                Difference = IC_Val2-IC_Val1;
+            }
+            else if(IC_Val1 > IC_Val2) {
+                Difference = (0xffff - IC_Val1) + IC_Val2;
+            }
+            Distance = Difference * .034/2;
+            bFirstCaptured = false; // set it back to false
+            // set polarity to rising edge
+            __HAL_TIM_SET_CAPTUREPOLARITY(htim, TIM_CHANNEL_1, TIM_INPUTCHANNELPOLARITY_RISING);
+            __HAL_TIM_DISABLE_IT(htim, TIM_IT_CC1);
+        }
+    } // if(htim->Channel == HAL_TIM_ACTIVE_CHANNEL_1)
+}
+*/
 
 // UART error callbacks
 // UartHandle: UART handle
